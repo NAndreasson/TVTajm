da75fc14d33e16f629615f79944f873baa0ff107
Niklas Andreasson <niklas@niftybits.se>
2024-01-15 14:22:31 +0000

    Extract DelayedLoader default delay to constant + comment

--- src/components/DelayedLoader.tsx
+++ src/components/DelayedLoader.tsx
@@ -5,13 +5,20 @@ import { useEffect, useState } from "react";
 // navigator.connection.effectiveType to determine whether to show the loader or
 // not.
 
+// Indended to test this component with React testing library but had some trouble with fake timers.
+
+const DEFAULT_DELAY = 250;
+
 // TODO: document, e.g. that delay is in MS
 interface DelayedLoaderProps {
   delay?: number;
   children: React.ReactNode;
 }
 
-export function DelayedLoader({ delay = 250, children }: DelayedLoaderProps) {
+export function DelayedLoader({
+  delay = DEFAULT_DELAY,
+  children,
+}: DelayedLoaderProps) {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
