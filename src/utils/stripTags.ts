9565d4b272a147f6444ca17de8c682d5c3468728
Niklas Andreasson <niklas@niftybits.se>
2024-01-15 12:39:51 +0000

    Remove stripTags since replace with Interweave

--- src/utils/stripTags.ts
+++
@@ -1,6 +0,0 @@
-// Not perfect by any means but work for our purpose.
-// For a more robust solution, one could consider using either
-// https://github.com/cure53/DOMPurify or DOMParser
-export function stripTags(html: string) {
-  return html.replace(/<[^>]+>/g, "");
-}
